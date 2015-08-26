6a3a005253c374bad2e67ae2e4347669c93470fd
Roy Rutto <royrutto@gmail.com>
2015-08-26 11:58:26 +0300

    RR: try out getZoom in FeatureLayer

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject prabhasp/leaflet-cljs "0.7.3"
- :description "osmtogeojson"
+(defproject royrutto/leaflet-cljs "0.7.3"
+ :description "leaflet-cljs"
   :url "https://leafletjs.com"
    :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"})
